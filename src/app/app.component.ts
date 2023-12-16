090e2dde2d64ed2fd423c58afbbe4ce0e68e2fdd
Zeko450 <moh-kach@hotmail.com>
2023-12-16 02:29:35 -0500

    home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'client';
 }
+  
+
