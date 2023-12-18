0d884c13a94cab2bbfdfaed77f5c4b22fe301095
Zeko450 <moh-kach@hotmail.com>
2023-12-18 09:00:20 -0500

    TextBox component , children of homepage component

---
+++ src/app/composant/homepage/textbox/textbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './textbox.component.html',
+  styleUrl: './textbox.component.css'
+})
+export class TextboxComponent {
+
+}
