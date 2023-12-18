51d44f10d61cfb9d24713b69cecd05c9ca4f2fe2
Zeko450 <moh-kach@hotmail.com>
2023-12-18 18:21:32 -0500

    added user dashboard and registration route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { AuthentifierUsagerComponent } from './composant/authentifier-usager/authentifier-usager.component';
 import { HomepageComponent } from './composant/homepage/homepage.component';
+import { DashboardComponent } from './composant/dashboard/dashboard.component';
+import { InscrireUsagerComponent } from './composant/inscrire-usager/inscrire-usager.component';
 
 
 export const routes: Routes = [
-    { path: 'accueil', component: HomepageComponent},
+    { path: 'accueil', component: HomepageComponent },
     { path: '', redirectTo: 'accueil', pathMatch: 'full' },
-    {path:'authentifierUsager', component: AuthentifierUsagerComponent}
+    { path: 'authentifierUsager', component: AuthentifierUsagerComponent },
+    { path: 'dashboard/:id', component: DashboardComponent },
+    { path: 'inscription' , component: InscrireUsagerComponent}
+
 ];
