581abf0f96a19a7c7a11c07e3738bd5d2e363a29
Zeko450 <moh-kach@hotmail.com>
2023-12-18 18:25:42 -0500

    authentification validation and navigation to next component

--- src/app/composant/authentifier-usager/authentifier-usager.component.ts
+++ src/app/composant/authentifier-usager/authentifier-usager.component.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { RestApiImmigrationService } from '../../service/rest-api-immigration.service';
 import { Compte } from '../../model/compte.model';
 import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-authentifier-usager',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, DashboardComponent],
   templateUrl: './authentifier-usager.component.html',
   styleUrl: './authentifier-usager.component.css'
 })
@@ -17,14 +18,21 @@ export class AuthentifierUsagerComponent {
 
   courriel: string = ""
   password: string = ""
+  authentifier = false;
+  accountId = ""
 
-  constructor(private restApi: RestApiImmigrationService, private router: ActivatedRoute, private redirect: Router) { }
+
+
+  constructor(private restApi: RestApiImmigrationService, private router: Router) { }
+
 
   authentifierUsager() {
-    console.log(this.courriel)
-    console.log(this.password)
     this.restApi.verifierUsager(this.courriel, this.password).subscribe((res) => {
-      console.log(res);
+      this.authentifier = res.isAuthenticated;
+      if (this.authentifier) {
+        this.accountId = res.account.account._id;
+        this.router.navigate(['/dashboard', this.accountId]);
+      }
     })
   }
 }
