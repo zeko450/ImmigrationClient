b82a16710b3d135996c8fd26e8209a4ca69def33
Zeko450 <moh-kach@hotmail.com>
2023-12-18 18:28:03 -0500

    minor change to handle error from the server

--- src/app/service/rest-api-immigration.service.ts
+++ src/app/service/rest-api-immigration.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Compte } from '../model/compte.model';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,15 @@ export class RestApiImmigrationService {
 
   //Authentifier Usager
   verifierUsager(courriel:string, password:string): Observable<any> {
-    return this.http.post<any>(this.URL + "accounts/verify", { courriel, password })
+    return this.http.post<any>(this.URL + "accounts/verify", { courriel, password }).pipe(
+      catchError((error: any) => {
+        console.error(error);
+        var err = throwError(() => new Error('Could not authentify user'))
+        // Handle the error as needed, e.g., show an alert
+        alert("Mot de passe ou courriel incorrect");
+        // Return an observable with a default value or rethrow the error
+        return err ;
+      })
+    )
   }
 }
