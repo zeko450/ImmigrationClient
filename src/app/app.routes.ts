0788e3fd8362abd85ffe16a721e15c13f3921d7d
Zeko450 <moh-kach@hotmail.com>
2023-12-18 08:56:52 -0500

    homepage and authentify components routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthentifierUsagerComponent } from './composant/authentifier-usager/authentifier-usager.component';
+import { HomepageComponent } from './composant/homepage/homepage.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'accueil', component: HomepageComponent},
+    { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+    {path:'authentifierUsager', component: AuthentifierUsagerComponent}
+];
